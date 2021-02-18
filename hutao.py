d4721dd8d0866f5019dc0e9519ffd9474a641590
Aidan San <muakasan@gmail.com>
2021-02-18 15:18:09 -0500

    Changed hutao

--- hutao.py
+++ hutao.py
@@ -24,17 +24,23 @@ na_frames = [14, 25, 51, 80, 116, 184]
 for i in range(6):
     print(sum(na_mv[:i+1])/(na_frames[i]+21)*60)
 '''
-n4_time = (80 + 21)/60 # From Hail + 21 for dash cancel
+# Frames counteed from myself
+skill_cast_time = 16/25 
+burst_cast_time = 55/25 
+# 2.84 seconds total
 
+n4c_time = 58/25
+n4c_casts = 2
+n1c_time = 27/25 
+n1c_casts = 5
 # E lasts for 9 seconds
-field_time = n4_time*4 + 1.5 + .3 # assuming 1.5 sec for burst cast and .3 for skill cast
+
 resist_down = 0
 
-low_hp = 0 # 0 when HP > 50% , 1 when HP is  < 50%
+low_hp = 1 # 0 when HP > 50% , 1 when HP is  < 50%
 use_bennet = 0 # 0 when excluding bennet atk buff, 1 when including
 
 cw_avg_stacks = 1
-dm_1_opp = .5
 liyue_chars = 2.0
 a4_uptime = 1
 base_hp = 13721
@@ -51,7 +57,8 @@ char_attr = AttrObj(base_atk=94, crit_rate=.05, crit_dmg=.788, dmg_bonus={DmgTag
 #archaic_attr = AttrObj(base_atk=565, atk_pct=.276) # archaic, lvl 90/90, phys procs later
 wt_attr = AttrObj(base_atk=401, crit_rate=.221, dmg_bonus={DmgTag.NORMAL: .48}) # white tassel R5, lvl 90/90
 bt_attr = AttrObj(base_atk=354, hp_pct=.469) # black tassel, lvl 90/90, assuming not slimes
-dm_attr = AttrObj(base_atk=454, crit_rate=.368, atk_pct=.16+.08*dm_1_opp) # deathmatch R1, lvl 90/90
+dm_solo_attr = AttrObj(base_atk=454, crit_rate=.368, atk_pct=.16+.08) # deathmatch R1, lvl 90/90 < 2 enemy
+dm_multi_attr = AttrObj(base_atk=454, crit_rate=.368, atk_pct=.16) # deathmatch R1, lvl 90/90 >= 2 enemy
 homa_attr = AttrObj(base_atk=608, hp_pct=.2, crit_dmg=.662) # Homa R1, lvl 90/90
 pjws0_attr = AttrObj(base_atk=674, crit_rate=.221) # Jade Winged Spear R1, 0 stacks, lvl 90/90
 pjws7_attr = AttrObj(base_atk=674, crit_rate=.221, atk_pct=.224, dmg_bonus={DmgTag.PYRO: .12}) # Jade Winged Spear R1, 7 stacks, lvl 90/90, remember to change from PYRO if not all abilies/attacks are pyro
@@ -61,7 +68,8 @@ pjws7_attr = AttrObj(base_atk=674, crit_rate=.221, atk_pct=.224, dmg_bonus={DmgT
 weapons = {
     "White Tassel": (wt_attr, cr_main_stats, False),
     "Black Tassel": (bt_attr, cr_main_stats, False),
-    "Deathmatch (Solo 50% time)": (dm_attr, cd_main_stats, False), 
+    "Deathmatch (Solo)": (dm_solo_attr, cd_main_stats, False), 
+    "Deathmatch (Multi)": (dm_multi_attr, cd_main_stats, False), 
     "Homa": (homa_attr, cr_main_stats, True),
     "Jade Winged Spear (0 stacks)": (pjws0_attr, cr_main_stats, False),
     "Jade Winged Spear (7 stacks)": (pjws7_attr, cr_main_stats, False)
@@ -89,17 +97,32 @@ for weapon_name, weapon in weapons.items():
     bennet_atk_bonus = bennet_base_atk * .784
     tot_attr.flat_atk += bennet_atk_bonus * use_bennet
     
-    na_ca_dmg = calc_avg_crit_dmg_obj(tot_attr, (.6447 + .6635 + .8394 + .9026), [DmgTag.PYRO, DmgTag.NORMAL], enemy_resist_pct=.1-resist_down)*4 # 4 N4's, remember to separate damage tags for normal and charged
+    n4_mv = .6447 + .6635 + .8394 + .9026
+    n1_mv = .6647
+    charge_mv = 1.8695
 
-    print(na_ca_dmg)
+    charge_dmg = calc_avg_crit_dmg_obj(tot_attr, charge_mv, [DmgTag.PYRO, DmgTag.CHARGED], enemy_resist_pct=.1-resist_down)
+
+    n4c_dmg = calc_avg_crit_dmg_obj(tot_attr, n4_mv, [DmgTag.PYRO, DmgTag.NORMAL], enemy_resist_pct=.1-resist_down)*n4c_casts # 2 N4C's
+    n4c_dmg += charge_dmg*n4c_casts
+    #print(n4c_dmg)
+
+    n1c_dmg = calc_avg_crit_dmg_obj(tot_attr, n1_mv, [DmgTag.PYRO, DmgTag.NORMAL], enemy_resist_pct=.1-resist_down)*n1c_casts # 5 N1C's
+    n1c_dmg += charge_dmg*n1c_casts
+    #print(n1c_dmg)
+
     skill_dmg = calc_avg_crit_dmg_obj(tot_attr, .896, [DmgTag.PYRO, DmgTag.SKILL], enemy_resist_pct=.1-resist_down)*2 # 2 blood blossoms
-    print(skill_dmg)
+    #print(skill_dmg)
 
     burst_dmg = calc_avg_crit_dmg_obj(tot_attr, 4.994 if low_hp else 3.9952, [DmgTag.PYRO, DmgTag.BURST], enemy_resist_pct=.1-resist_down)*1
-    print(burst_dmg)
+    #print(burst_dmg)
 
-    tot_dmg = na_ca_dmg + skill_dmg + burst_dmg  
+    tot_n4c_dmg = n4c_dmg + skill_dmg + burst_dmg  
+    tot_n1c_dmg = n1c_dmg + skill_dmg + burst_dmg
 
-    dps = tot_dmg/field_time
-    print("DPS:", dps)
+    n4c_dps = tot_n4c_dmg/(burst_cast_time + skill_cast_time + n4c_casts*n4c_time)
+    n1c_dps = tot_n1c_dmg/(burst_cast_time + skill_cast_time + n1c_casts*n1c_time)
+
+    print("N4C DPS:", n4c_dps)
+    print("N1C DPS:", n1c_dps)
     print()

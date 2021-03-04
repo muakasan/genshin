540f8c1ddb571707aca5cfe2730cdd09da1c686c
Aidan San <muakasan@gmail.com>
2021-03-04 03:49:02 -0500

    updated hutao artifact sets

--- hutao_artifact_sets.py
+++ hutao_artifact_sets.py
@@ -10,12 +10,15 @@ cw2no2_set_effects = AttrObj(dmg_bonus={DmgTag.PYRO: .15, DmgTag.BURST: .20})
 # artifact effects, should vape, vape bonus
 artifact_sets = {
     "Crimson Witch 4": (cw_set_effects, True, .15),
-    "Thundering Fury": (tf_set_effects, False, 0),
     "Crimson Witch 2, Glad 2": (cw2glad2_set_effects, True, 0), 
     "Crimson Witch 2, Wanderer's 2": (cw2wt2_set_effects, True, 0),
     "Crimson Witch 2, Noblesse 2": (cw2no2_set_effects, True, 0),
+    "Thundering Fury": (tf_set_effects, False, 0),
 }
 
 
 for set_name, (set_effects, should_vape, vape_bonus) in artifact_sets.items():
-    print(set_name, n3cq_dps(dm_attr, cd_main_stats, artifact_substats, set_effects, low_hp=1, vape_bonus=vape_bonus))
+    _, dmg, dur = n3cq_dps(dm_attr, cd_main_stats, artifact_substats, set_effects, vape=should_vape, low_hp=0, vape_bonus=vape_bonus)
+    if not should_vape:
+        dmg += tf_react_dmg(ReactionType.OL, em=99, bonus=.40, char_lvl=80, enemy_resist_pct=.1)*6
+    print(set_name, dmg/dur)

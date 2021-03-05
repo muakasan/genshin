4818fb09da2bc98a766274c60bc94ada54d19dbd
Aidan San <muakasan@gmail.com>
2021-03-05 17:39:27 -0500

    updated lw

--- hutao_artifact_sets.py
+++ hutao_artifact_sets.py
@@ -13,7 +13,7 @@ lw_set_effects = AttrObj(dmg_bonus={DmgTag.PYRO: .35})
 # artifact effects, should vape, vape bonus
 artifact_sets = {
     "Crimson Witch 4": (cw_set_effects, True, .15),
-    "Lavawalker (100% Pyro aura)": (lw_set_effects, True, 0),
+    "Lavawalker (100% Pyro aura)": (lw_set_effects, False, 0),
     "Crimson Witch 2, Wanderer's 2": (cw2wt2_set_effects, True, 0),
     "Retracing Bolide (100% Shield)": (bolide_set_effects, True, 0),
     "Crimson Witch 2, Glad 2": (cw2glad2_set_effects, True, 0), 
@@ -25,6 +25,6 @@ artifact_sets = {
 
 for set_name, (set_effects, should_vape, vape_bonus) in artifact_sets.items():
     _, dmg, dur = n3cq_dps(dm_attr, cd_main_stats, artifact_substats, set_effects, vape=should_vape, low_hp=1, vape_bonus=vape_bonus)
-    if not should_vape:
-        dmg += tf_react_dmg(ReactionType.OL, em=99, bonus=.40, char_lvl=80, enemy_resist_pct=.1)*6
+    #if not should_vape:
+    #    dmg += tf_react_dmg(ReactionType.OL, em=99, bonus=.40, char_lvl=80, enemy_resist_pct=.1)*6
     print(set_name, dmg/dur)

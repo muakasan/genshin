272acc5f96df8e117a4b9cac87a1a0777e1c03d3
Aidan San <muakasan@gmail.com>
2021-03-05 17:43:47 -0500

    more artifact set updates

--- hutao_artifact_sets.py
+++ hutao_artifact_sets.py
@@ -10,21 +10,21 @@ cw2_set_effects = AttrObj(dmg_bonus={DmgTag.PYRO: .15})
 bolide_set_effects = AttrObj(dmg_bonus={DmgTag.NORMAL: .40, DmgTag.CHARGED: .40})
 lw_set_effects = AttrObj(dmg_bonus={DmgTag.PYRO: .35})
 
-# artifact effects, should vape, vape bonus
+# artifact effects, should vape, vape bonus, add OL
 artifact_sets = {
-    "Crimson Witch 4": (cw_set_effects, True, .15),
-    "Lavawalker (100% Pyro aura)": (lw_set_effects, False, 0),
-    "Crimson Witch 2, Wanderer's 2": (cw2wt2_set_effects, True, 0),
-    "Retracing Bolide (100% Shield)": (bolide_set_effects, True, 0),
-    "Crimson Witch 2, Glad 2": (cw2glad2_set_effects, True, 0), 
-    "Crimson Witch 2, Noblesse 2": (cw2no2_set_effects, True, 0),
-    "Crimson Witch 2": (cw2_set_effects, True, 0),
-    #  "Thundering Fury": (tf_set_effects, False, 0),
+    "Crimson Witch 4": (cw_set_effects, True, .15, False),
+    "Lavawalker (100% Pyro aura)": (lw_set_effects, False, 0, False),
+    "Crimson Witch 2, Wanderer's 2": (cw2wt2_set_effects, True, 0, False),
+    "Retracing Bolide (100% Shield)": (bolide_set_effects, True, 0, False),
+    "Crimson Witch 2, Glad 2": (cw2glad2_set_effects, True, 0, False), 
+    "Crimson Witch 2, Noblesse 2": (cw2no2_set_effects, True, 0, False),
+    "Crimson Witch 2": (cw2_set_effects, True, 0, False),
+    #"Thundering Fury": (tf_set_effects, False, 0, True),
 }
 
 
-for set_name, (set_effects, should_vape, vape_bonus) in artifact_sets.items():
+for set_name, (set_effects, should_vape, vape_bonus, should_OL) in artifact_sets.items():
     _, dmg, dur = n3cq_dps(dm_attr, cd_main_stats, artifact_substats, set_effects, vape=should_vape, low_hp=1, vape_bonus=vape_bonus)
-    #if not should_vape:
-    #    dmg += tf_react_dmg(ReactionType.OL, em=99, bonus=.40, char_lvl=80, enemy_resist_pct=.1)*6
+    if should_OL:
+        dmg += tf_react_dmg(ReactionType.OL, em=99, bonus=.40, char_lvl=80, enemy_resist_pct=.1)*6
     print(set_name, dmg/dur)
